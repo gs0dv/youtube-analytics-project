aea19643220414f8e7a0d20ddd3e91135eb40567
gs0dv <gs0dv@main.ru>
2023-05-26 18:01:04 +0300

    added tests in file test_channel.py

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -2,7 +2,40 @@
 from src.channel import Channel
 
 channel = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+highload = Channel('UCwHL6WHUarjGfUM_586me8w')
 
 
 def test_print_info():
     assert isinstance(channel.print_info(), str) == True
+
+
+def test_str():
+    assert str(channel) == 'MoscowPython (https://www.youtube.com/channel/UC-OVMPlMA3-YCIeg4z5z23A)'
+
+
+def test_add():
+    assert channel + highload == 100500
+
+
+def test_sub():
+    assert channel - highload == -48500
+
+
+def test_gt():
+    assert (channel > highload) == False
+
+
+def test_ge():
+    assert (channel >= highload) == False
+
+
+def test_lt():
+    assert (channel < highload) == True
+
+
+def test_le():
+    assert (channel <= highload) == True
+
+
+def test_eq():
+    assert (channel == highload) == False
